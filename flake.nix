f6382d770bba37d35c307a9d36a2f95096025c58
Bradley Saul <bradleysaul@fastmail.com>
2024-05-23 11:15:29 -0400

    add agda

--- flake.nix
+++ flake.nix
@@ -11,7 +11,9 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+
     in {
+      
       packages.documents.intension = pkgs.stdenv.mkDerivation {
         name = "intension";
         src = ./.;
@@ -27,8 +29,24 @@
         cp intension.docx $out
         '';
       };
+
       devShells.default =  pkgs.mkShell {
         buildInputs = [
+
+          (pkgs.agda.withPackages (p: [
+              (p.standard-library.overrideAttrs (oldAttrs: {
+                version = "2.0";
+                src = pkgs.fetchFromGitHub {
+                  repo = "agda-stdlib";
+                  owner = "agda";
+                  rev = "v2.0";
+                  # sha256 = pkgs.lib.fakeSha256;
+                  sha256 = "TjGvY3eqpF+DDwatT7A78flyPcTkcLHQ1xcg+MKgCoE=";
+                };
+              }))
+        ]))
+
+
           # Documentation/writing tools
           pkgs.pandoc
         ];
